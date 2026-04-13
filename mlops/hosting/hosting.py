7ebfeacb587684fda2f499162705fc4aceab6f98
AndrewKiran25 <director.ashok888@gmail.com>
2026-04-13 11:38:52 +0000

    first commit

--- mlops/hosting/hosting.py
+++ mlops/hosting/hosting.py
@@ -9,7 +9,7 @@ if not hf_token:
 api = HfApi(token=hf_token)
 
 api.upload_folder(
-    folder_path="/content/mlops/deployment",     # the local folder containing your files
+    folder_path="mlops/deployment",     # the local folder containing your files
     repo_id="Andrew2505/Employee-Promotion",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
